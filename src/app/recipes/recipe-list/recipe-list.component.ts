2e88a03e7fb39421022f5e311f8d6a408f422e09
Alok Goyal <alokg1019@gmail.com>
2018-03-06 21:39:45 +0530

    created recipe list using recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A test recipe','Test recipe desciption','http://assets.kraftfoods.com/recipe_images/opendeploy/52512_640x428.jpg'),
+    new Recipe('A test recipe','Test recipe desciption','http://assets.kraftfoods.com/recipe_images/opendeploy/52512_640x428.jpg')
+
+  ];
   constructor() { }
 
   ngOnInit() {
