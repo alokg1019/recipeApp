7de62aad63c9d3bbb24e383332843a40e92d7f70
Alok Goyal <alokg1019@gmail.com>
2018-03-10 22:02:13 +0530

    recipe ingredient displayed

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,23 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService{
     
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Rolls','Recipe for roll','http://assets.kraftfoods.com/recipe_images/opendeploy/52512_640x428.jpg'),
-        new Recipe('Pizza Recipe','Recipe for pizza','https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg')   
+        new Recipe('Rolls',
+        'Recipe for roll','http://assets.kraftfoods.com/recipe_images/opendeploy/52512_640x428.jpg',
+        [
+            new Ingredient('Potato', 4),
+            new Ingredient('Dough', 1)
+        ]),
+        new Recipe('Pizza Recipe','Recipe for pizza','https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg',
+        [
+            new Ingredient('Pizza Dough',1),
+            new Ingredient('Cheese', 1)
+        ])   
       ];
 
       getRecipes()
