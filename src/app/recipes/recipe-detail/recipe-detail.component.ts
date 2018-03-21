a9095f41f2d6ad336b9269fce80a863aeb5f10b4
Alok Goyal <alokg1019@gmail.com>
2018-03-21 11:09:33 +0530

    child route for recipe edit added

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -15,7 +15,7 @@ export class RecipeDetailComponent implements OnInit {
   id: number;
 
   constructor(private recipeService: RecipeService,
-              private route:ActivatedRoute) { }
+              private route:ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     // const id = this.route.snapshot.component['id'];
@@ -31,4 +31,7 @@ export class RecipeDetailComponent implements OnInit {
       this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
+  onEditRecipe(){
+    this.router.navigate(['edit'],{relativeTo : this.route});
+  }
 }
