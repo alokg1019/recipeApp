8ed010abd762a08922738f7bbc75cd1a53ff8576
Alok Goyal <alokg1019@gmail.com>
2018-03-26 01:55:55 +0530

    delete and cancel btn working

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { NgForm, FormGroup, FormControl, FormArray, Validators } from '@angular/Forms';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
+import { relative } from 'path';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -15,7 +16,9 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm : FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService,
+    private router:Router
+  ) { }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -82,6 +85,9 @@ export class RecipeEditComponent implements OnInit {
     else{
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+
+      this.onCancel();
+
   }
 
   onAddIngredient(){
@@ -91,6 +97,10 @@ export class RecipeEditComponent implements OnInit {
       'amount': new FormControl(null,[Validators.required,Validators.pattern(/^[0-9]+[0-9]*$/)])
       })
     )
+  }
+
+  onCancel(){
+      this.router.navigate(['../'],{relativeTo: this.route});
   }
 
 }
