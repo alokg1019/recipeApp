b1801d74f5fce9432ed6172c78b6098e8e22a105
Alok Goyal <alokg1019@gmail.com>
2018-03-31 18:05:03 +0530

    setting up firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   loadedFeature = 'recipe';
+
+  ngOnInit(){
+      firebase.initializeApp({
+        apiKey: "AIzaSyABN39zijmNla5k5eu4sJRJ9sw8qzip3j0",
+        authDomain: "recipe-app-learning.firebaseapp.com"
+      })
+  }
 
   onNavigate(feature: string){
       console.log('Event received: '+ feature);
